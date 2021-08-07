20f5cb5e43288e96e1a917f34cd4c7210e1b559c
FrankApiyo <franklineapiyo@gmail.com>
2021-08-07 14:55:52 +0300

    Add page one: with tex area for writing sql commands

--- src/app/hello.cljs
+++ src/app/hello.cljs
@@ -4,9 +4,28 @@
    ["sql.js/dist/sql-wasm.js" :as sqljs]))
 
 (defn hello []
-  (let [SQL (r/atom nil)]
+  (let [SQL (r/atom nil)
+        db (r/atom nil)
+        query "CREATE TABLE employees(id integer,  name text,designation text,manager integer,hired_on date,salary  integer,commission float,dept integer);"]
     (.then
-     (sqljs {:locateFile "http://localhost:3000/sql-wasm.wasm"})
-     #(reset! SQL %))
+     (.then
+      (sqljs {:locateFile "http://localhost:3000/sql-wasm.wasm"})
+      #(reset! SQL %))
+     #(let [Database (.-Database @SQL)]
+       (reset! db (Database.))))
     (fn []
-      [:h1 "SQL Tutorial"])))
+      [:div
+       [:h1 "SQL Tutorial"]
+       [:p "A database has already been created, run the following query to create a table"]
+       [:p query]
+       [:textarea {:rows 4 :cols 50
+                   :on-key-press (fn [e]
+                                   (when (= 13 (.-charCode e))
+                                     (try
+                                       (.run @db
+                                             (.-value
+                                              (.-target e)))
+                                       (catch js/Error e (js/console.log (.-message e))))))}
+        ]
+       ;; visually show what's in the database
+       ])))
